c190467d651dc0e6a536732086b8fab4235ced7d
Joon Lee <joonhlee100@gmail.com>
2018-11-07 10:52:16 -0800

    Added test cases to test for null

--- Lab6/equality/TestCases.java
+++ Lab6/equality/TestCases.java
@@ -56,4 +56,39 @@ public class TestCases
 
       assertNotEquals(one.hashCode(), two.hashCode());
    }
+
+   @Test
+   public void testExercise5()
+   {
+      final CourseSection one = new CourseSection(null, null, 35,
+              LocalTime.of(9, 40), LocalTime.of(11, 0));
+      final CourseSection two = new CourseSection(null, null, 35,
+              LocalTime.of(9, 40), LocalTime.of(11, 0));
+
+      assertTrue(one.equals(two));
+      assertTrue(two.equals(one));
+   }
+
+   @Test
+   public void testExercise6()
+   {
+      final CourseSection one = new CourseSection("CSC", "203", 35,
+              LocalTime.of(9, 10), LocalTime.of(10, 0));
+      final CourseSection two = new CourseSection(null, null, 35,
+              LocalTime.of(1, 10), LocalTime.of(2, 0));
+
+      assertFalse(one.equals(two));
+      assertFalse(two.equals(one));
+   }
+    @Test
+    public void testExercise7()
+    {
+        final CourseSection one = new CourseSection("CSC", null, 35,
+                LocalTime.of(9, 10), LocalTime.of(10, 0));
+        final CourseSection two = new CourseSection("CSC", "203", 34,
+                LocalTime.of(9, 10), LocalTime.of(10, 0));
+
+        assertNotEquals(one.hashCode(), two.hashCode());
+    }
+
 }
